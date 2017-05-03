f6e2c77ccb6598c0cee57588ed229122a5cf8eda
yeosu0107 <yeosu0107@naver.com>
2017-05-03 17:03:00 +0900

    충전소 정보 읽어오기
    필수 정보 읽어오기 성공

--- main.py
+++ main.py
@@ -7,9 +7,22 @@ serviceKey="4lmWp814ERUDuydXPhoe%2FcCMA8%2BAdBGDoCEnTrCRixmHXdNe63pJDHI9NJueNefS
 
 
 def urlBuilder(server, key):
-    data='?'+'serviceKey='+key+'&numOfRows='+str(10)+'&pageNo='+str(1)
+    data='?'+'serviceKey='+key+'&numOfRows='+str(30)+'&pageNo='+str(1)
     request=server+data
     return request
+
+def LoadPoint(item):
+    dic = {}
+    dic["name"] = item.find("csNm").text
+    dic["addr"]=item.find("addr").text
+    dic["type"]=item.find("chargeTp").text
+    dic["cpName"]=item.find("cpNm").text
+    dic["state"]=item.find("cpStat").text
+    dic["type2"]=item.find("cpTp").text
+    dic["lat"]=item.find("lat").text
+    dic["longi"]=item.find("longi").text
+
+    return dic
 
 url=urlBuilder(server, serviceKey)
 
@@ -18,11 +31,13 @@ data=urllib.request.urlopen(url).read()
 tree=ElementTree.fromstring(data)
 itemElement=list(tree.iter("item"))
 
-dic={}
+
+list=[]
 
 for item in itemElement:
     a1=item.find("addr").text
-    dic[a1]=1
+    tmp = LoadPoint(item)
+    list.append(tmp)
 
-for i in dic:
+for i in list:
     print(i)
